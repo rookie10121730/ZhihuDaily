a45ee51baff1ba62c24193ea29442dda546fa587
hzxubowen <seven_forjob@163.com>
2016-08-09 11:55:58 +0800

    改变了启动动画页面 但是footer那还有待改进

--- app/src/main/java/com/seven/hzxubowen/zhihudaily/ui/activity/SplashActivity.java
+++ app/src/main/java/com/seven/hzxubowen/zhihudaily/ui/activity/SplashActivity.java
@@ -1,59 +1,61 @@
 package com.seven.hzxubowen.zhihudaily.ui.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.graphics.Typeface;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.Window;
 import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
+import android.view.animation.BounceInterpolator;
 import android.widget.ImageView;
-import android.widget.TextView;
-
+import android.widget.RelativeLayout;
 import com.seven.hzxubowen.zhihudaily.R;
 
-import gr.net.maroulis.library.EasySplashScreen;
-
 public class SplashActivity extends AppCompatActivity {
+
+    RelativeLayout sfv;
+    ImageView img;
+    ObjectAnimator tranOa;
+    ObjectAnimator alphaOa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EasySplashScreen config = new EasySplashScreen(SplashActivity.this)
-                .withFullScreen()
-                .withTargetActivity(HomeActivity.class)
-                .withSplashTimeOut(3000)
-                .withBackgroundResource(android.R.color.holo_blue_light)
-                .withFooterText("Copyright By Seven")
-                .withBeforeLogoText("知乎日报")
-                .withLogo(R.mipmap.logo)
-                .withAfterLogoText("每天三次，每次七分钟");
-
-
-        //set your own animations
-        myCustomTextViewAnimation(config.getFooterTextView());
-
-        //customize all TextViews
-        Typeface font = Typeface.createFromAsset(getAssets(), "scyahei.ttf");
-        config.getAfterLogoTextView().setTypeface(font);
-        config.getFooterTextView().setTextColor(Color.WHITE);
-        config.getFooterTextView().setTextSize(25);
-        config.getFooterTextView().setTypeface(font);
-
-        //create the view
-        View easySplashScreenView = config.create();
-
-        setContentView(easySplashScreenView);
+        setContentView(R.layout.ac_splash);
+        sfv = (RelativeLayout) findViewById(R.id.splash_footer);
+        img = (ImageView) findViewById(R.id.splash_body);
+        initAnimation();
     }
 
-    private void myCustomTextViewAnimation(TextView tv){
-        Animation animation=new TranslateAnimation(0,0,480,0);
-        animation.setDuration(2000);
-        tv.startAnimation(animation);
+    AnimatorListenerAdapter adapter = new AnimatorListenerAdapter() {
+        @Override
+        public void onAnimationEnd(Animator animation) {
+            super.onAnimationEnd(animation);
+            if(animation == tranOa){
+                img.setVisibility(View.VISIBLE);
+                alphaOa.start();
+            }else{
+                Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    };
+
+    void initAnimation(){
+
+        alphaOa = ObjectAnimator.ofFloat(img, "alpha", 0.0f, 1.0f);
+        alphaOa.setDuration(2500);
+        alphaOa.addListener(adapter);
+
+        tranOa = ObjectAnimator.ofFloat(sfv, "translationY", Animation.RELATIVE_TO_SELF, 480, 0);
+        tranOa.setDuration(1000);
+        tranOa.addListener(adapter);
+        tranOa.start();
+
     }
 }
